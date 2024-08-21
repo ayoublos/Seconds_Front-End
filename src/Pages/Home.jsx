ec590e4361acbefc42b3ae6c695ae707b0577e46
JorgeLG87 <jorge85_6@hotmail.com>
2024-08-20 20:39:59 -0400

    Forgot to change the name of the component in the Home.jsx

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -3,14 +3,14 @@ import { Outlet } from'react-router-dom'
 import { useState, useEffect } from 'react';
 
 //COMPONENTS
-import Seconds from '../Components/Foods/Foods';
+import Foods from '../Components/Foods/Foods';
 
 export default function Home() {
 
   return (
     <div>
       <Outlet />
-      <Seconds />
+      <Foods />
     </div>
   )
 }
