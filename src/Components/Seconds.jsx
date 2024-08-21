08f57c11091586b9a6073cc52457e1329900407f
JorgeLG87 <jorge85_6@hotmail.com>
2024-08-20 20:20:28 -0400

    Created the Second and Seconds components. Also created the CSS for them

---
+++ src/Components/Seconds.jsx
@@ -0,0 +1,63 @@
+import "../CSS/Seconds.css";
+import { useState, useEffect } from 'react';
+
+//COMPONENTS
+import Second from '../Components/Second';
+
+
+
+export default function Seconds() {
+
+    const [ restaurants, setRestaurants ] = useState([]);
+    const [ foods, setFoods ] = useState([]);
+    const [ seconds, setSeconds ] = useState([]);
+
+    //FOODS ARRAY
+    useEffect(() => {
+        fetch("http://localhost:3005/food")
+        .then(response => response.json())
+        .then(res => setFoods(res))
+        .catch(err => console.error(err))
+    },[])
+    
+
+    //RESTAURANTS ARRAY
+    useEffect(() => {
+        fetch("http://localhost:3005/restaurant")
+        .then(response => response.json())
+        .then(res => setRestaurants(res))
+        .catch(err => console.error(err))
+    },[])
+
+
+    //SECONDS ARRAY
+    useEffect(() => {
+        setSeconds(foods.filter(food => food.seconds === true))
+    },[foods]);
+
+    //-------------------------TESTING PURPOSES---------------------------------
+    useEffect(() => {
+        console.log(foods,"Foods Array")
+    }, [foods])
+
+    useEffect(() => {
+        console.log(restaurants, "Restaurants Array")
+    }, [restaurants])
+
+    useEffect(() => {
+        console.log(seconds, "Seconds Array")
+    }, [seconds])
+    //--------------------------------------------------------------------------
+
+
+    return (
+        <>
+            <p className="seconds-title">Your Seconds</p>
+            {foods.filter(food => food.seconds === true).map(second => {
+                return (
+                   <Second id={second.id} key={second.id} />
+                )
+            })}     
+        </>
+    )
+}
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import { Outlet } from'react-router-dom'
+import { useState, useEffect } from 'react';
+
+//COMPONENTS
+import Seconds from '../Components/Seconds';
 
 export default function Home() {
+
   return (
     <div>
       <Outlet />
+      <Seconds />
     </div>
   )
 }
